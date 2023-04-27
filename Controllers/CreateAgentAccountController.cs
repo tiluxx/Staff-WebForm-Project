8a27d0d4c873f1eb67509bd27545cd1468c0a4e8
JLBiLong <trantobaolong0802@gmail.com>
2023-04-27 22:57:42 +0700

    Add CreateAgentAccount function.

--- Controllers/CreateAgentAccountController.cs
+++ Controllers/CreateAgentAccountController.cs
@@ -9,6 +9,79 @@ namespace Agent_WebForm_Prodject.Controllers
 {
     public class CreateAgentAccountController : Controller
     {
-        
+        // GET: CreateAgentAccount
+        public ActionResult Index()
+        {
+            Agent agent = new Agent();
+            List<Agent> res = agent.SelectAgentQuery();
+            ViewBag.AgentList = res;
+            return View();
+        }
+
+        // GET: CreateAgentAccount/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // GET: CreateAgentAccount/ViewAgentAccount
+        public ActionResult ViewAgentAccount(string agentId)
+        {
+            ViewBag.Data = agentId;
+            return View();
+        }
+
+        private string GenerateAccountUsername(string agentName)
+        {
+            Random res = new Random();
+            string str = "0123456789";
+            int size = 4;
+
+            string result = "";
+            for (int i = 0; i < size; i++)
+            {
+                int x = res.Next(str.Length);
+                result += str[x];
+            }
+
+            string agentNameStandardlized = agentName.Replace(" ", "").ToUpper();
+            return agentNameStandardlized + result;
+        }
+
+        private string GenerateAccountPassword(string agentID)
+        {
+            Random res = new Random();
+            string str = "abcdefghijklmnopqrstuvwxyz0123456789";
+            int size = 5;
+
+            string result = "";
+            for (int i = 0; i < size; i++)
+            {
+                int x = res.Next(str.Length);
+                result += str[x];
+            }
+
+            return agentID + "_" + result;
+        }
+
+        [HttpPost]
+        public ActionResult CreateNewAgent()
+        {
+            string agentId = Request.Form["AgentId"];
+            string agentName = Request.Form["AgentName"];
+            string agentPhone = Request.Form["AgentPhone"];
+            string agentEmail = Request.Form["AgentEmail"];
+            string agentAddress = Request.Form["AgentAddress"];
+            Agent agent = new Agent();
+            agent.AddAgentQuery(agentId, agentName, agentEmail, agentPhone, agentAddress);
+
+            string username = GenerateAccountUsername(agentName);
+            string password = GenerateAccountPassword(agentId);
+            AgentAccount agentAccount = new AgentAccount();
+            agentAccount.AddAgentAcQuery(agentId, username, password, false);
+
+            ViewBag.Message = "Create new agent and agent account successfully";
+            return View("Result");
+        }
     }
 }
